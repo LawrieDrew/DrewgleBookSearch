63bcdaff27c3e519363683ea08290574ea4fe813
LawrieDrew <laurenn.drew@gmail.com>
2021-07-05 13:48:08 -0400

    added files, moved around files, trying to get structure correct to get index.html to load properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { GoogleBookSearch } from 'react-native-google-books';
+import SavedBooks from 'saved books placeholder'
 
-
-import Discover from "./pages/Discover";
-import About from "./pages/About";
-import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -16,10 +13,8 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
+          <Route exact path="/" component={GoogleBookSearch} />
+          <Route exact path="/saved" component={SavedBooks} />
         </Wrapper>
         <Footer />
       </div>
